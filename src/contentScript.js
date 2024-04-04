e5aac4bbea08068edd8c23dbd1b19b35349385e2
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-04-04 21:32:26 +0530

    fix new twitter css

--- src/contentScript.js
+++ src/contentScript.js
@@ -13,8 +13,9 @@
 const svgPlay = `<svg class="r-4qtqp9 r-yyyyoo r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1srniue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M8 5v14l11-7z"></path></svg>`;
 const svgPause = `<svg class="r-4qtqp9 r-yyyyoo r-50lct3 r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1srniue" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path></svg>`;
 // @ts-ignore
-window.utterances = [];
 let windowurl = window.location.href;
+let boundaryEventListener = null;
+let utterance = null;
 
 const playTweet = (event) => {
   // on click change svg to pause
@@ -27,15 +28,20 @@ const playTweet = (event) => {
     } else {
       // closest div with data-testid="tweet"
       console.log('playing new tweet');
-      let tweetDiv = playButton.parentElement.parentElement.parentElement.parentElement.querySelector('[data-testid="tweetText"]');
+      let tweetDiv = null;
+      // keep finding parent element data-testid="tweetText" until it is found
+      while(tweetDiv === null) {
+        tweetDiv = playButton.querySelector('[data-testid="tweetText"]');
+        playButton = playButton.parentElement;
+      }
       let tweetCtr = tweetDiv.closest('[data-testid="cellInnerDiv"]');
       let tweetAuthor = tweetCtr.querySelector('a').href;
       let tweetText = tweetDiv.innerText;
-      let utterance = new SpeechSynthesisUtterance(tweetText);
+      utterance = new SpeechSynthesisUtterance(tweetText);
+      console.log('utterance', utterance);
       // @ts-ignore
-      window.utterances.push(utterance);
       window.speechSynthesis.speak(utterance);
-      utterance.addEventListener('boundary', (event) => {
+      boundaryEventListener = (event) => {
         let word = event.charIndex;
         let wordLength = event.charLength;
         let wordStart = word;
@@ -45,12 +51,9 @@ const playTweet = (event) => {
         let tweetTextWord = tweetText.slice(wordStart, wordEnd);
         let tweetTextAfterWord = tweetText.slice(wordEnd);
         // scroll to tweetDiv 
-        window.scrollTo({
-          behavior: 'smooth',
-          top: tweetDiv.getBoundingClientRect().top + window.scrollY - 100,
-        });
-        tweetDiv.innerHTML = `${tweetTextBeforeWord}<span style="background-color: #38bdf8;color: white;border: 3px solid #0c4a6e;border-radius: 5px;">${tweetTextWord}</span>${tweetTextAfterWord}`;
-      });
+        tweetDiv.innerHTML = `${tweetTextBeforeWord}<span style="background-color: #38bdf8;color: white;border: 3px solid #333;border-radius: 5px;">${tweetTextWord}</span>${tweetTextAfterWord}`;
+      };
+      utterance.addEventListener('boundary', boundaryEventListener);
 
 
       // @ts-ignore
@@ -82,7 +85,10 @@ const playTweet = (event) => {
 // on div click remove all utterances
 const stopAll = () => {
   console.log('stop all');
-  window.speechSynthesis.cancel();
+  // destroy all utterances and stop speaking
+  window.speechSynthesis.pause();
+  utterance?.removeEventListener('boundary', boundaryEventListener);
+
   let playButtons = document.querySelectorAll('[aria-label="Play"]');
   // remove all utterances
   // @ts-ignore
@@ -95,7 +101,7 @@ const stopAll = () => {
 const main = () => {
   // white svg for play
 
-  const buttonContainer = `<div class="css-1dbjc4n r-18u37iz r-1h0z5md"><div aria-label="Play" role="button" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr" data-testid="like"><div dir="ltr" class="css-901oao r-1awozwy r-1bwzh9t r-6koalj r-37j5jr r-a023e6 r-16dba41 r-1h0z5md r-rjixqe r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0"><div class="css-1dbjc4n r-xoduu5"><div class="css-1dbjc4n r-1niwhzg r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-xf4iuw r-1ny4l3l r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg"></div>${svgPlay}</div></div></div></div>`
+  const buttonContainer = `<div class="css-1dbjc4n r-18u37iz r-1h0z5md" style="cursor: pointer; transform: rotate(0deg) scale(1) translate3d(0px, 0px, 0px); justify-content: space-around; width: 50px; text-align: center; display: flex;align-items: center;"><div aria-label="Play" role="button" tabindex="0" class="css-18t94o4 css-1dbjc4n r-1777fci r-bt1l66 r-1ny4l3l r-bztko3 r-lrvibr" data-testid="like"><div dir="ltr" class="css-901oao r-1awozwy r-1bwzh9t r-6koalj r-37j5jr r-a023e6 r-16dba41 r-1h0z5md r-rjixqe r-bcqeeo r-o7ynqc r-clp7b1 r-3s2u2q r-qvutc0"><div class="css-1dbjc4n r-xoduu5"><div class="css-1dbjc4n r-1niwhzg r-sdzlij r-1p0dtai r-xoduu5 r-1d2f490 r-xf4iuw r-1ny4l3l r-u8s1d r-zchlnj r-ipm5af r-o7ynqc r-6416eg"></div>${svgPlay}</div></div></div></div>`
 
   // add event listener for buttonContainer on click playTweet
 
@@ -160,3 +166,22 @@ window.addEventListener(
   },
   true
 );
+
+const destroyAll = () => {
+  window.speechSynthesis && window.speechSynthesis.cancel();
+  utterance?.removeEventListener('boundary', boundaryEventListener);
+  utterance = null;
+  boundaryEventListener = null;
+  // reset speech synthesis
+  stopAll();
+}
+// on refresh stop all
+window.addEventListener('beforeunload', () => {
+  // destroy all utterances and stop speaking
+  destroyAll();
+});
+
+// when url changes stop all
+window.addEventListener('hashchange', () => {
+  destroyAll();
+});
